b89496ecd9bf0488ad8dbb904f111e84380ccccc
Reddbooy <eduardogarroj04@gmail.com>
2026-06-24 03:55:11 -0700

    Delete Index.php

--- Examen-extraordinario/parcial-1-poo/practica-1/Index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Usuario.php';
-
-$usuario = new Usuario("Eduardo Rojas", "eduardogarroj04@gmail.com");
-
-echo "Nombre: " . $usuario->getNombre() . "<br>";
-echo "Correo: " . $usuario->getCorreo();
-?>
