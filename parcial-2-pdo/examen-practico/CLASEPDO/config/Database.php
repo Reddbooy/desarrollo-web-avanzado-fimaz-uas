3a6f6a55e5d9ff7f1e35241d80f43555a957bc51
Reddbooy <eduardogarroj04@gmail.com>
2026-06-24 03:50:42 -0700

    Update Database.php

--- parcial-2-pdo/examen-practico/CLASEPDO/config/Database.php
+++ parcial-2-pdo/examen-practico/CLASEPDO/config/Database.php
@@ -1,27 +1,37 @@
 <?php
+
+//Jesus Eduardo Garcia Rojas
+
+namespace config;
+use PDO;
+use PDOException;
+
 class Database {
     private $host = "localhost";
-    private $db = "database"; 
-    private $user = "root";
+    private $dbname = "phppdobd";
+    private $username = "root";
     private $password = "";
-    private $charset = "utf8mb4";
     private $connection;
-
+    
     public function __construct(){
-        try {
-            $dns = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
-            
-            $this->connection = new PDO($dns, $this->user, $this->password);
+        try{
+            $dsn = "mysql:host={$this->host};
+            dbname={$this->dbname}; charset=utf8mb4";
+
+            $this->connection = new PDO($dsn, $this->username,
+            $this->password);
+
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
+        } catch (PDOException $e){
+            die("Error de conexión: ". $e->getMessage());
+        }  
     }
 
     public function getConnection(){
         return $this->connection;
     }
 }
+
 ?>
